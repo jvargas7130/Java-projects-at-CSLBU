dff824a6215580c38593ca24b4e96f840c68346f
jvargas7130 <jvargas7130@gmail.com>
2016-10-09 14:41:34 -0700

    finish without java docks

--- PokeMindReader/src/PokeTester.java
+++ PokeMindReader/src/PokeTester.java
@@ -1,3 +1,10 @@
+import java.awt.Point;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class PokeTester {
 
@@ -5,82 +12,174 @@ public class PokeTester {
 		menu();
 	}
 
+	public static Computer readFile(int game){
+		boolean file = false;
+		Computer c = null;
+		int choose = 0;
+		int savedGame = 0;
+
+
+		File B = new File("Beginner.Dat");
+		File V = new File("Veteran.Dat");
+		File F = null;
+		
+		if (game == 1) {
+			if (B.exists()) {
+				System.out.println("Would you like to continue beginner game?");
+				System.out.println("1.Yes");
+				System.out.println("2.No");
+				 savedGame= CheckInput.checkInt(1, 2);
+				 if(savedGame == 1){
+					 file = true;
+					 	F = B;
+				 }else if(savedGame == 2){
+					 
+				 }
+				
+			
+			} else {
+				System.out.println("New Game");
+			}
+		} else if (game == 2) {
+			
+				if (V.exists()) {
+					System.out.println("Would you like to continue Veteran game?");
+					System.out.println("1.Yes");
+					System.out.println("2.No");
+					 savedGame= CheckInput.checkInt(1, 2);
+					 if(savedGame == 1){
+						 file = true;
+						 	F = V;
+					 }else if(savedGame == 2){
+						 
+					 }
+				
+				} else {
+					System.out.println("New Game");
+				}
+			
+		}
+
+		if (file) {
+
+	
+				try {
+					ObjectInputStream in = new ObjectInputStream(new FileInputStream(F));
+					c = (Computer) in.readObject();
+					in.close();
+				} catch (IOException e) {
+					System.out.println("Error processing file.");
+				} catch (ClassNotFoundException e) {
+					System.out.println("Could not find class.");
+				}
+
+	
+		} else {// new game
+			
+			 c = new Computer();
+			
+		}
+
+		return c;
+
+	}
+	
+	
+
+
 	public static void menu() {
 		boolean done = false;
-		String pattern = "";
+	
 		int pWins = 0;
 		int cWins = 0;
 
-		Computer c = new Computer();
-		int computer = 0;
 
+		int computer = 0;
+		String write = "";
+		// reads in saved file
 		System.out.print("Welcome to Pokemon Mind Reader");
 		System.out.println("Choose Difficulty");
 		System.out.println("1. Beginner");
 		System.out.println("2. Veteran");
-		int choose = CheckInput.checkInt(1, 2);
 
-		while (!done) {//beginner
+		int choose = CheckInput.checkInt(1, 2);
+		Computer c = readFile(choose);
+		
+		
+		String pattern = "";
+		
+		try {
+			pattern = c.getPattern();
+
+		} catch (NullPointerException e) {
+			pattern = "" ;
+		
+		}
+
+	
+
+		while (!done) {// beginner
 
 			if (choose == 1) {
-
+				System.out.println("beginner");
+				
+				write = "Beginner.Dat";
 				if (pattern.length() == 5) {
 
 					pattern = pattern.substring(1);
-					c.storePattern(new Pattern(pattern));
-
+					computer = c.makePrediction(pattern);
+				} else {
+					int randChoice = (int) (Math.random() * 3) + 1;
+					computer = randChoice;
 				}
-				//repeating menu
-				computer = c.makePrediction(pattern);
-				System.out.println("Choose Pokemon");
-				System.out.println("1. Fire");
-				System.out.println("2. Water");
-				System.out.println("3. Grass");
-				
-				//player chooses
-				int player = CheckInput.checkInt(1, 3);
-				pattern = playGame(player, pattern, computer);
-				int win = winner(player, c, pattern, computer);
-				
-				if (win == 1) {// player wins
-					pWins++;
-				} else if (win == -1) {// computer wins
-					cWins++;
-				}
-				showPercentage(cWins, pWins);
 
-			} else if (choose == 2) {//veteran
-
+			} else if (choose == 2) {// veteran
+				write = "Veteran.Dat";
 				if (pattern.length() == 9) {
 
 					pattern = pattern.substring(1);
 					c.storePattern(new Pattern(pattern));
 
+				} else {
+					int randChoice = (int) (Math.random() * 3) + 1;
+					computer = randChoice;
 				}
-				
-				//repeating menu
-				computer = c.makePrediction(pattern);
-				System.out.println("Choose Pokemon");
-				System.out.println("1. Fire");
-				System.out.println("2. Water");
-				System.out.println("3. Grass");
-				
-				//player chooses 
-				int player = CheckInput.checkInt(1, 3);
-				pattern = playGame(player, pattern, computer);//plays game and returens pattern
-				int win = winner(player, c, pattern, computer);//chooses winner and returns value
-				
+
+			}
+
+			// repeating menu
+			computer = c.makePrediction(pattern);
+			System.out.println("Choose Pokemon");
+			System.out.println("1. Fire");
+			System.out.println("2. Water");
+			System.out.println("3. Grass");
+			System.out.println("4. quit");
+
+			// player chooses
+			int player = CheckInput.checkInt(1, 4);
+			if (player == 4) {
+				writeGame(c, write);
+				System.out.println("Saving..........");
+				done = true;
+			} else {
+				pattern = playGame(player, pattern, computer);// plays game and
+																// returens
+																// pattern
+				int win = winner(player, c, pattern, computer);// chooses winner
+																// and returns
+																// value
+
 				if (win == 1) {// player wins
 					pWins++;
 				} else if (win == -1) {// computer wins
 					cWins++;
 				}
-				showPercentage(cWins, pWins);
+				showPercentage(cWins, pWins);// shows percentage
 
+				writeGame(c, write);// save game every round
 			}
 
 		}
-
 	}
 
 	public static String playGame(int player, String pattern, int computer) {
@@ -150,6 +249,16 @@ public class PokeTester {
 
 		System.out.println();
 
+	}
+
+	public static void writeGame(Computer c, String f) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(f));
+			out.writeObject(c);
+			out.close();
+		} catch (IOException e) {
+			System.out.println("Error processing file.");
+		}
 	}
 
 }
