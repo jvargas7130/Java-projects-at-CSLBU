55cf21ac7cb28acb2408655c4849a183e24c87bc
jvargas7130 <jvargas7130@gmail.com>
2016-10-16 09:07:13 -0700

    project 3 done

---
+++ Test/src/Cube.java
@@ -0,0 +1,16 @@
+
+public class Cube extends Rectangle {
+
+	private int h;
+	public Cube(int l, int w, int h){
+		super(l,w);
+	}
+	
+	int getHeight(){
+		return h;
+	}
+	
+	int getArea(){
+		
+	}
+}
